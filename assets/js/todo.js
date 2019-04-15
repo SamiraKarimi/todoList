cfdd61f12bbdd31fa8dbf5643833a6a3ff631415
Samira Karimi <samira.ka006@gmail.com>
2019-04-15 15:35:51 -0500

    Deleting todos

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,3 +1,12 @@
 $("li").click(function() {
   $(this).toggleClass("completed");
 });
+//click on X to delete Todo
+$("span").click(function(e) {
+  $(this)
+    .parent()
+    .fadeOut(500, function() {
+      $(this).remove();
+    });
+  event.stopPropagation();
+});
