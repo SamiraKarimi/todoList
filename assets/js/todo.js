3097b9e7b1444605c43fead8233dc1df59cbc4aa
Samira Karimi <samira.ka006@gmail.com>
2019-04-15 22:19:06 -0500

    Creating Todos

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,12 +1,20 @@
-$("li").click(function() {
+$("ul").on("click", "li", function() {
   $(this).toggleClass("completed");
 });
 //click on X to delete Todo
-$("span").click(function(e) {
+$("ul").on("click", "span", function(e) {
   $(this)
     .parent()
     .fadeOut(500, function() {
       $(this).remove();
     });
   event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+  if (event.which === 13) {
+    var val = $(this).val();
+    $(this).val("");
+    $("ul").append("<li><span> X </span>" + val + "</li>");
+  }
 });
